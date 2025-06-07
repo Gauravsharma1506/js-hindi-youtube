cc607c48c74e95f5c1a15cf3603b249a69bb492f
Gauravsharma1506 <hiteshim15@gmail.com>
2025-06-07 02:26:49 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score=null
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber=Number(score)
+// console.log(valueInNumber)
+// console.log(typeof(valueInNumber))
+
+
+
+// "33" => 33
+// "33abc" => NaN
+// true  => 1
+
+
+
+
+let isLoggedIn=""
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof(stringNumber))
