1142b33f87f5ccc2a90a14c814b30b32d22172d5
Gauravsharma1506 <hiteshim15@gmail.com>
2025-06-07 13:27:25 +0000

    conversion operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,5 +24,20 @@ let booleanIsLoggedIn=Boolean(isLoggedIn)
 
 let someNumber=33
 let stringNumber=String(someNumber)
-console.log(stringNumber)
-console.log(typeof(stringNumber))
+// console.log(stringNumber)
+// console.log(typeof(stringNumber))
+
+// ****************************operations***********************
+let value=3
+let negValue=-value
+console.log(negValue)
+
+let str1="hello"
+let str2=" hitesh"
+let str3=str1+str2
+console.log(str3)
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)//output 122
+console.log(1+2+"2")//output 32
