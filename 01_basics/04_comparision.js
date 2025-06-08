91e36818a30557a80f7b02e797bf7df10e51d48c
Gauravsharma1506 <hiteshim15@gmail.com>
2025-06-08 06:28:09 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,15 @@
+console.log("2">1)
+
+
+/*
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)
+
+console.log(undefined==0)
+console.log(undefined>0)
+console.log(undefined>=0)
+*/      //try to avoid these type of code
+
+//===
+console.log("2"===1)// this will check strictly  means datatype also
